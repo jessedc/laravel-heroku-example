cdde5c427f7c4527367af9ea20b78b8e20b2f30f
Jesse Collis <jesse@jcmultimedia.com.au>
2017-09-15 12:22:55 +1000

    default failed job database is pgsql

--- config/queue.php
+++ config/queue.php
@@ -78,7 +78,7 @@ return [
     */
 
     'failed' => [
-        'database' => env('DB_CONNECTION', 'mysql'),
+        'database' => env('DB_CONNECTION', 'pgsql'),
         'table' => 'failed_jobs',
     ],
 
